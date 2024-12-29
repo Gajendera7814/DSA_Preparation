89d13eec269acb97aa37134a65268e3577a4f329
Gajendera Kumar <gajenderakumar806@gmail.com>
2024-12-29 22:23:19 +0530

    Right-Angled Triangle Star Pattern

---
+++ Pattern Problems/01_Right_Angled_Triangle.js
@@ -0,0 +1,59 @@
+/*
+    1. "Right-Angled Triangle Star Pattern" or "Pyramid Pattern"
+    <----------------------------------------------------------->
+*/
+
+let str = "";
+
+for(let i = 0; i < 5; i++){
+    for(let j = 0; j < 5; j++){
+        if (j <= i) {
+            str = str.concat("* ");
+        }
+    }
+    str = str.concat("\n");
+};
+console.log(str);
+
+/*
+    Time Complexity: O(n2)
+    Space Complexity: O(n2)
+*/
+
+/*
+
+    Output:-
+
+    *
+    * *       
+    * * *     
+    * * * *   
+    * * * * *
+*/
+
+
+
+/*<<<<<<<<<<<<<<<<---------------------- Write in a better way ------------------------------------>>>>>>>>>>>>>>>>*/
+
+let newStr = [];
+
+for (let i = 0; i < 5; i++) {
+    let line = "";
+    for (let j = 0; j <= i; j++) {
+        line += "* ";
+    }
+    newStr.push(line);
+};
+console.log(newStr); // [ '* ', '* * ', '* * * ', '* * * * ', '* * * * * ' ]
+console.log(newStr.join("\n"));
+
+/*
+
+    Output:-
+
+    *
+    * *       
+    * * *     
+    * * * *   
+    * * * * *
+*/
