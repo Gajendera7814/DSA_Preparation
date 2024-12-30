4b17440c79588f628d7227775cec1d6d9695684c
Gajendera Kumar <gajenderakumar806@gmail.com>
2024-12-31 00:38:14 +0530

    Check if a number is Palindrome or Not

--- 03_Basic Math Problems/03_Check_Palindrome_or_Not.js
+++ 03_Basic Math Problems/03_Check_Palindrome_or_Not.js
@@ -6,7 +6,7 @@
 
     A palindrome is a number that reads the same backward as forward.
 
-    For example, 121, 1331, and 4554 are palindromes because they remain the same when their digits are reversed.
+    For example :- 121, 1331, and 4554 are palindromes because they remain the same when their digits are reversed.
 */
 
 const palindromeOrNot = (n) => {
