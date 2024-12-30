50cc2d17b2fc91372b9adbf07f5c99b305af41d1
Gajendera Kumar <gajenderakumar806@gmail.com>
2024-12-30 23:28:29 +0530

    Print All Divisors of a number

--- Basic Math Problems/05_Print_All_Divisors.js
+++ Basic Math Problems/05_Print_All_Divisors.js
@@ -5,6 +5,9 @@
     E.g - 36 --> 1, 2, 3, 4, 6, 9, 12, 18, 36
 */
 
+
+/*<-------------------------------------------- Brute Force Approach ------------------------------------------------>*/
+
 const printDivisors = (n) => {
     for(let i = 1; i < n; i++){
         if(n % i === 0){
@@ -17,5 +20,28 @@ console.log(printDivisors(36));
 
 /*
     Time Complexity: O(n), where n is the input number.
+    Space Complexity: O(1).
+*/
+
+
+
+/*<------------------------------------ Optimized Approach (Using Square Root) -------------------------------------->*/
+
+const printDivisorsOptimized = (n) => {
+    for (let i = 1; i <= Math.sqrt(n); i++) {
+        if (n % i === 0) {
+            console.log(i); // Print the smaller divisor
+            if (i !== n / i) {
+                console.log(n / i); // Print the larger divisor
+            }
+        }
+    }
+};
+console.log(printDivisorsOptimized(36));
+
+
+
+/*
+    Time Complexity: O(Square Root of n), where n is the input number.
     Space Complexity: O(1).
 */
