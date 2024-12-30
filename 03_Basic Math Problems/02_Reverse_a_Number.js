cdab976f41f3a2cf78be33fdd0cf46112afb6d4a
Gajendera Kumar <gajenderakumar806@gmail.com>
2024-12-31 00:37:33 +0530

    Reverse a Number

--- 03_Basic Math Problems/02_Reverse_a_Number.js
+++ 03_Basic Math Problems/02_Reverse_a_Number.js
@@ -3,13 +3,11 @@
     <-------------->    <--------------->
 
     Example 1:
-        Input: N = 123
-        Output: 321
+        Input: N = 123, Output: 321
         Explanation: The reverse of 123 is 321
 
     Example 2:
-        Input: N = 234
-        Output: 432
+        Input: N = 234, Output: 432
         Explanation: The reverse of 234 is 432
 */
 
