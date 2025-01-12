bee878e2a60930615de1be9c9a5de976419122f9
Gajendera Kumar <gajenderakumar806@gmail.com>
2025-01-12 22:43:47 +0530

    Generate Valid Parentheses

---
+++ 04_Recursion/08_Generate_Valid_Parentheses.js
@@ -0,0 +1,78 @@
+/*
+    Generate Valid Parentheses
+    <------------------------>
+
+    Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+    Input: n = 3,   Output: ["((()))","(()())","(())()","()(())","()()()"]
+    
+    Input: n = 1,   Output: ["()"]
+*/
+
+const generateValidParenthesis = (n, openingCount, closingCount, ans = "") => {
+    /* Positive Base Case */
+    if (openingCount === n && closingCount === n) {
+        console.log(ans);
+        return;
+    }
+
+    /* Negative Base Case */
+    if (openingCount > n || closingCount > openingCount) {
+        return;
+    }
+
+    generateValidParenthesis(n, openingCount + 1, closingCount, ans + "(");
+
+    generateValidParenthesis(n, openingCount, closingCount + 1, ans + ")");
+};
+generateValidParenthesis(3, 0, 0);
+
+/*
+    Output -
+
+    "((()))"
+    "(()())"
+    "(())()"
+    "()(())"
+    "()()()"
+*/
+
+
+const generateValidParenthes = (n, openingCount, closingCount, ans = "", result = []) => {
+    /* Positive Base Case */
+    if (openingCount === n && closingCount === n) {
+        result.push(ans);
+        return result;
+    }
+
+    /* Negative Base Case */
+    if (openingCount > n || closingCount > openingCount) {
+        return result;
+    }
+
+    generateValidParenthes(n, openingCount + 1, closingCount, ans + "(", result);
+    generateValidParenthes(n, openingCount, closingCount + 1, ans + ")", result);
+
+    return result;
+};
+
+const output = generateValidParenthes(3, 0, 0);
+console.log("Output:", output); /* Output :- Output: ["((()))","(()())","(())()","()(())","()()()"] */
+
+/*
+                    Recursive Tree Diagram
+                    <--------------------->
+
+                              ""
+                            /    \
+                          "("     ")"
+                         /         \
+                      "(("         "()"
+                     /    \        /   \
+                 "((("   "(()"   "()("  "())"
+                  /         \
+             "((()"       "(()("
+               /             \
+          "((())"          "(()())"
+
+*/
